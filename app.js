cc2f669c3e78ecded8acd1e6e7801a01c804d08b
Delta Student <ShaikhUmarsu843129@gmail.com>
2025-09-25 00:31:34 +0530

    Updated code

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing");  //importing the Listing model
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
 const cors = require("cors");  //connet http request from hoppscotch
-const {listingSchema} = require("./schema.js");
+const session = require("express-session");
+const flash = require("connect-flash")
+
+
+const listings = require("./routes/listing.js");
+const reviews = require("./routes/review.js");
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,89 +35,34 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());  //to avoid cors error while connecting from hoppscotch
 
 
+
+const sessionOptions = {
+    secret: "mysupersecrecode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 *1000,
+        maxAge:  7 * 24 * 60 * 1000,
+        httpOnly: true,
+    }
+};
+
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
 }); 
 
-const validateListing = (req, res, next) => {
-    let {error} = listingSchema.validate(req.body); 
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(",");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-}
+app.use(session(sessionOptions));
+app.use(flash());
 
-//index route
-app.get("/listings", wrapAsync(async(req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", {allListings}); 
-}));
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+})
 
-//New Route
-app.get("/listings/new", (req, res) => {
-    res.render("listings/new.ejs");
-});
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews);  
 
-//Show route
-app.get("/listings/:id", wrapAsync(async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id)
-    res.render("listings/show.ejs", {listing});
-}));
-
-//Create route
-app.post(
-    "/listings",
-    validateListing, 
-    wrapAsync(async (req, res, next) => {
-      const newListing = new Listing(req.body.listing);
-      await newListing.save();
-      res.redirect("/listings");
-    })
-);
-
-//Edit Route
-app.get("/listings/:id/edit", wrapAsync(async(req, res) => {
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing });
-}));
-
-//Update Route
-app.put(
-    "/listings/:id", 
-    validateListing,
-    wrapAsync(async(req, res) => {
-      let {id} = req.params;
-      await Listing.findByIdAndUpdate(id, {...req.body.listing});
-      res.redirect(`/listings/${id}`);
-    })
-);
-
-//Delete Route
-app.delete("/listings/:id", wrapAsync(async(req, res) => {
-    let {id} = req.params;
-    let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
-    res.redirect("/listings");
-}));
-
-//This route is used for testing purposes.
-// app.get("/testListing", async (req, res) => {  //new route to test listing model
-//     let sampleListing = new Listing({   //Creates a new Listing object with sample data:
-//         title: "My new Villa",
-//         description: "By the beach",
-//         price: 1200,
-//         location: "Calangute, Goa",
-//         country: "India"
-//     });
-
-//     await sampleListing.save(); //saving the sample listing to the database
-//     console.log("Sample listing saved"); // after saving the sample listing, we will log a message 
-//     res.send("Sample listing created and saved to the database"); // sending a response to the client
-// })
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
